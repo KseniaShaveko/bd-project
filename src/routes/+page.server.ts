294eb6c250b89d8674c86a5172ef71a96fe6701c
KseniaShaveko <kshaveko04@gmail.com>
2024-05-25 14:47:53 +1000

    Добавлено разбиение по страницам(пагинация)

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -8,6 +8,7 @@ export async function load({ url }) {
     (url.searchParams.get('sortBy') as keyof Phone) || 'id';
   const sortOrder = url.searchParams.get('sortOrder');
   const manufacturer = url.searchParams.get('manufacturer') || '';
+  const page = Number(url.searchParams.get('page') ?? 1);
 
   // const filteredArray = data.phones.filter((value) =>
   //   search ? value.body.includes(search) || value.title.includes(search) : true
@@ -16,7 +17,9 @@ export async function load({ url }) {
   //   sortBy === 'desc' ? b.id - a.id : a.id - b.id
   // );
 
-  const phones = await db.query.phoneTable.findMany({
+  const pageSize = 10;
+
+  const foundPhones = await db.query.phoneTable.findMany({
     orderBy:
       sortOrder === 'desc' ? desc(phoneTable[sortBy]) : asc(phoneTable[sortBy]),
     where: (value, { like, and, eq }) => {
@@ -31,13 +34,34 @@ export async function load({ url }) {
     },
   });
 
+  const offset = (page - 1) * pageSize;
+  const phones = foundPhones.slice(offset, offset + pageSize);
+
   const allPhones = await db.query.phoneTable.findMany();
 
   const manufacturers = new Set(allPhones.map((phone) => phone.manufacturer));
+  const pagesTotal = Math.ceil(foundPhones.length / pageSize);
+
+  const prevPage = new URL(url);
+  prevPage.searchParams.set('page', String(page - 1));
+  const nextPage = new URL(url);
+  nextPage.searchParams.set('page', String(page + 1));
+  const firstPage = new URL(url);
+  firstPage.searchParams.set('page', String(1));
+  const lastPage = new URL(url);
+  lastPage.searchParams.set('page', String(pagesTotal));
 
   return {
     phones,
     manufacturers,
+    pagination: {
+      pagesTotal,
+      page,
+      prevPage: prevPage.href,
+      nextPage: nextPage.href,
+      firstPage: firstPage.href,
+      lastPage: lastPage.href,
+    },
   };
 }
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,19 +2,19 @@
   import type { Phone } from '$lib/db/schema.js';
 
   export let data;
+  $: pagination = data.pagination;
   const sortBy: { value: keyof Phone; label: string }[] = [
     { label: 'Название', value: 'name' },
     { label: 'Цена', value: 'price' },
     { label: 'Год', value: 'year' },
   ];
-
 </script>
 
 <form method="get">
   <input name="search" type="text" placeholder="Поиск..." />
   <select name="sortBy" id="sortBy">
     {#each sortBy as option}
-    <option value={option.value}>{option.label}</option>
+      <option value={option.value}>{option.label}</option>
     {/each}
   </select>
   <select name="sortOrder" id="sortOrder">
@@ -24,7 +24,7 @@
   <select name="manufacturer" id="manufacturer">
     <option value="">Любой производитель</option>
     {#each data.manufacturers as option}
-    <option value={option}>{option}</option>
+      <option value={option}>{option}</option>
     {/each}
   </select>
   <button type="submit">Поиск</button>
@@ -37,3 +37,18 @@
     </li>
   {/each}
 </ul>
+
+<div>
+  {#if pagination.page > 1}
+    <a href={pagination.firstPage}>{1}&lt;&lt;</a>
+  {/if}
+  {#if pagination.page > 2}
+    <a href={pagination.prevPage}>{pagination.page - 1}&lt;</a>
+  {/if}
+  {#if pagination.page < pagination.pagesTotal}
+    <a href={pagination.nextPage}>&gt;{pagination.page + 1}</a>
+  {/if}
+  {#if pagination.page < pagination.pagesTotal - 1}
+    <a href={pagination.lastPage}>&gt;&gt;{pagination.pagesTotal}</a>
+  {/if}
+</div>
